dc8de26d0a52b97d046a9ae57bd9551d5cc34fe2
= <=>
2021-12-16 21:18:34 -0300

    Modelando o contacts

--- app/resources/contacts.py
+++ app/resources/contacts.py
@@ -1,13 +1,22 @@
-from flask_restful import Resource
-
+from flask_restful import Resource, marshal
+from app.resources.models import Contact
+from app import request, db
+from app.resources.schemas import contact_field
 
 class Contacts(Resource):
     def get(self):
-        return {"message": "Hello World!"}
+        contacts = Contact.query.all()
+        return marshal(contacts, contact_field, "contacts")
     
     def post(self):
-        pass
-
+        payload = request.only(["name", "cellphone"])
+        name = payload["name"]
+        cellphone = payload["cellphone"]
+        contact = Contact(name, cellphone)
+        db.session.add(contact)
+        db.session.commit()
+        return marshal(contact, contact_field, "contact")    
+        
     def put(self):
         pass
     
